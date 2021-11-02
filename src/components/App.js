d4a4a52d589d76aa66c24c1356427bf3426ffa8b
Andrew Busel <buselax@gmail.com>
2021-11-02 11:26:56 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,31 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [displayedTasks, setDisplayedTasks] = useState(TASKS)
+  const [selectedCat, setSelectedCat]= useState('All')
+  const [newCat, setNewCat] = useState('Code')
+  const [newTask, setNewTask] = useState('')
+
+  function handleCatSelection(cat){
+    setSelectedCat(cat)
+  }
+  
+
+  function handleDelete(deletedTaskText){
+    setDisplayedTasks(displayedTasks.filter((task)=>{
+     return task.text !== deletedTaskText
+    }))
+  }
+  
+  function addTask(task){
+    setDisplayedTasks([...displayedTasks, task])
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCat={selectedCat} handleCatSelection={handleCatSelection}/>
+      <NewTaskForm categories={CATEGORIES} newCat={newCat} setNewCat={setNewCat} newTask={newTask} setNewTask={setNewTask} addTask={addTask}/>
+      <TaskList displayedTasks={displayedTasks} handleDelete={handleDelete} selectedCat={selectedCat}/>
     </div>
   );
 }
